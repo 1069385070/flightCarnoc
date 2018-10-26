4031b6a8a2f54abeb7e07afdfc3a01f0e359e605
heyang <1069385070@qq.com>
2018-10-26 16:24:25 +0800

    换血

---
+++ src/main/java/com/carnoc/flight/statisticAnalysis/service/A.java
@@ -0,0 +1,4 @@
+package com.carnoc.flight.statisticAnalysis.service;
+
+public class A {
+}
---
+++ src/main/test/com/carnoc/flight/test/demo.java
@@ -0,0 +1,41 @@
+package com.carnoc.flight.test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class demo {
+    private static Logger logger = LogManager.getLogger(demo.class);
+    public static void main(String[] args) {
+//        JedisPool jedisPool = new JedisPool("192.168.232.128",6379);
+//        Jedis jedis = jedisPool.getResource();
+//        String ping = jedis.ping();
+//        System.out.println(ping);
+//
+//        Set<String> keys = jedis.keys("*");
+//        for (String key : keys) {
+//            System.out.println(key);
+//        }
+//        String s = jedis.get("com.heyang.ssm.dao.UserDao.getAllUser");
+//        System.out.println(s);
+
+//        ApplicationContext applicationContext=new ClassPathXmlApplicationContext(new String[] {"spring-core.xml"});
+//        SqlSessionFactory sqlSessionFactory = applicationContext.getBean("sqlSessionFactory");
+//        UserService userService = new UserServiceImpl();
+//        System.out.println(System.currentTimeMillis());
+//        for (int i=1;i<1000000;i++){
+//            User userManager = new User();
+//            userManager.setUserName("T202hahaha"+i);
+//            userManager.setUserPassword(""+i);
+//            userService.addUser(userManager);
+//        }
+        System.out.println(System.currentTimeMillis());
+        System.out.println("哈哈哈哈哈");
+        System.out.println("jahahahah");
+        System.out.println("不更新");
+
+        System.out.println("提交");
+        System.out.println("李鹏飞修改了");
+        logger.info("何洋log");
+
+    }
+}
